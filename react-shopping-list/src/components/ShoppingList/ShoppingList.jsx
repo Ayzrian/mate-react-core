417e6d8824371ddb2f54c53155e67ca56ccf56c9
Nazarii Romankiv <ayzrian@gmail.com>
2025-02-15 20:04:44 +0200

    How to do conditional styles (Start)

--- react-shopping-list/src/components/ShoppingList/ShoppingList.jsx
+++ react-shopping-list/src/components/ShoppingList/ShoppingList.jsx
@@ -1,8 +1,9 @@
 import { ShoppingListItem } from "../ShoppingListItem/ShoppingListItem";
+import "./ShoppingList.scss"
 
 export function ShoppingList() {
     return (
-        <ul>
+        <ul className="ShoppingList">
             <ShoppingListItem item={{name: "Tomato", quantity: 3}} />
             <ShoppingListItem item={{name: "Cucumber", mustHave: true}} />
             <ShoppingListItem item={{name: "Sprite"}} />
--- react-shopping-list/src/components/ShoppingListItem/ShoppingListItem.jsx
+++ react-shopping-list/src/components/ShoppingListItem/ShoppingListItem.jsx
@@ -1,7 +1,8 @@
+import "./ShoppingListItem.scss"
 
 export function ShoppingListItem({ item: { name, quantity = 1, mustHave }}) {
     return (
-        <li>
+        <li className="ShoppingListItem">
             {name} - {quantity}{mustHave && <strong>M*</strong>}
         </li>
     );
