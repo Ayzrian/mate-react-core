c3bbf6f3e96b749d39ef467e4542af0c2eeeb899
Nazarii Romankiv <ayzrian@gmail.com>
2025-02-26 21:20:25 +0200

    How to sort a list

--- react-shopping-list/src/App.jsx
+++ react-shopping-list/src/App.jsx
@@ -7,18 +7,19 @@ import { ShoppingListPanel } from "./components/ShoppingListPanel/ShoppingListPa
 
 const items = [
   {id: 1, name: "Tomato", quantity: 3},
-  {id: 2, name: "Cucumber", mustHave: true},
-  {id: 3, name: "Sprite"},
-  {id: 4, name: "Mellon"}
+  {id: 2, name: "Cucumber", quantity: 1, mustHave: true},
+  {id: 3, name: "Sprite", quantity: 5, mustHave: true},
+  {id: 4, name: "Mellon", quantity: 1}
 ]
 
 
 function App() {
   const [list, setList] = useState(items);
   const [mustHaveFilter, setMustHaveFilter] = useState(false);
+  const [sortBy, setSortBy] = useState('');
 
   const handleSubmit = (item) => {
-    setList([...list, { ...item, id: Math.floor(Math.random() * 100000) + 1 }]);
+    setList([...list, { ...item, id: Math.floor(Math.random() * 100000) + 1, quantity: 1 }]);
   }
 
   const handleItemDelete = (itemId) => {
@@ -28,12 +29,31 @@ function App() {
   }
 
   const resultingList = useMemo(() => {
-    if (mustHaveFilter) {
-      return list.filter((item) => item.mustHave);
+    const results = mustHaveFilter ? list.filter((item) => item.mustHave) : list;
+
+    if (!sortBy) {
+      return results;
     }
 
-    return list;
-  }, [list, mustHaveFilter])
+    switch(sortBy) {
+      case 'mustHave':
+        return [...results].sort((a, b) => {
+          if (a.mustHave && b.mustHave) {
+            return 0
+          }
+
+          if (a.mustHave && !b.mustHave) {
+            return -1;
+          }
+
+          return 1;
+        });
+      case 'quantity':
+        return [...results].sort((a, b) => b.quantity - a.quantity);
+      default:
+        throw new Error('Unknown field name!');
+    }
+  }, [list, mustHaveFilter, sortBy]) 
 
   return (
     <>
@@ -44,7 +64,11 @@ function App() {
       <ShoppingListForm onSubmit={handleSubmit} />
 
       <br/>
-      <ShoppingListPanel mustHaveFilter={mustHaveFilter} onMustHaveFilterChange={setMustHaveFilter} />
+      <ShoppingListPanel
+        mustHaveFilter={mustHaveFilter}
+        onMustHaveFilterChange={setMustHaveFilter}
+        onSortByChange={setSortBy}
+      />
       <br/>
 
       <ShoppingList list={resultingList} onItemDelete={handleItemDelete}/>
--- react-shopping-list/src/components/ShoppingListPanel/ShoppingListPanel.jsx
+++ react-shopping-list/src/components/ShoppingListPanel/ShoppingListPanel.jsx
@@ -1,6 +1,6 @@
 
 
-export function ShoppingListPanel({ mustHaveFilter, onMustHaveFilterChange,  }) {
+export function ShoppingListPanel({ mustHaveFilter, onMustHaveFilterChange, onSortByChange  }) {
     const handleCheckBoxChange = (event) => {
         onMustHaveFilterChange(event.target.checked);
     }
@@ -8,5 +8,12 @@ export function ShoppingListPanel({ mustHaveFilter, onMustHaveFilterChange,  }) 
     return (<div>
         <label>Only Must Have:</label>
         <input type="checkbox" value={mustHaveFilter} onChange={handleCheckBoxChange} />
+
+        <div>
+            Sort by:
+            <button onClick={() => onSortByChange('mustHave')}>Must Have</button>
+            <button onClick={() => onSortByChange('quantity')}>Quantity</button>
+            <button onClick={() => onSortByChange('')}>Reset</button>
+        </div>
     </div>)
 }
